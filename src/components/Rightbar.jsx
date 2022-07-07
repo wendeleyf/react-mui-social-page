c3d699429528564d5f0efd55af36981f85623852
Wendeley <wendeleyf@gmail.com>
2022-07-07 17:18:53 -0300

    feat: adicionando lista de amigos ao componente Rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
 import React from 'react'
 
 function Rightbar() {
@@ -12,7 +12,29 @@ function Rightbar() {
         }
       }}>
         <Box position='fixed'>
-          
+          <Typography variant='h6' fontWeight={100}>Amigos online</Typography>
+          <AvatarGroup max={4}>
+            <Avatar
+              alt='John Doe'
+              src='https://material-ui.com/static/images/avatar/3.jpg'
+            />
+            <Avatar
+              alt='John Doe'
+              src='https://material-ui.com/static/images/avatar/4.jpg'
+            />
+            <Avatar
+              alt='John Doe'
+              src='https://material-ui.com/static/images/avatar/5.jpg'
+            />
+            <Avatar
+              alt='John Doe'
+              src='https://material-ui.com/static/images/avatar/6.jpg'
+            />
+            <Avatar
+              alt='John Doe'
+              src='https://material-ui.com/static/images/avatar/7.jpg'
+            />
+          </AvatarGroup>
         </Box>
     </Box>
   )
