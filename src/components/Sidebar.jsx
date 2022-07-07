71e90797c15147aded353705aec08db9b076653e
Wendeley <wendeleyf@gmail.com>
2022-07-07 15:50:30 -0300

    feat: adicionando componente de Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { Box } from '@mui/material'
+import { AccountBox, Group, Home, ManageAccounts, ModeNight, Person, Storefront } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 function Sidebar() {
@@ -11,7 +12,64 @@ function Sidebar() {
           xs: 'none', sm: 'block'
         }
       }}>
-        Sidebar
+        <List>
+          <ListItem>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary='Home' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#groups'>
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary='Grupos' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#marketplace'>
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary='Marketplace' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#settings'>
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary='Amigos' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#settings'>
+              <ListItemIcon>
+                <ManageAccounts />
+              </ListItemIcon>
+              <ListItemText primary='Configurações' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#settings'>
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary='Perfil' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component='a' href='#nightmode'>
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
     </Box>
   )
 }
